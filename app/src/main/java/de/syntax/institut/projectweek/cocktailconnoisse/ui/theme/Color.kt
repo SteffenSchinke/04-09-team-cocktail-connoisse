81484a4e7ab27f227a62ad11ca2d348c1ee576b4
Jacobmam <jacobmam1335@gmail.com>
2025-05-26 13:49:45 +0200

    App Colors

--- app/src/main/java/de/syntax/institut/projectweek/cocktailconnoisse/ui/theme/Color.kt
+++ app/src/main/java/de/syntax/institut/projectweek/cocktailconnoisse/ui/theme/Color.kt
@@ -9,3 +9,17 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val primary = Color(0xFF4F3933) // Jacob's Bogen Color & Bottom Color
+val secondary = Color(0xFFFCF3E1) // Background Color & Search Color
+val heart = Color(0xFF8D0909) // Favorite Heart Color
+val quickList = Color(0xFF1C8000) // Quick List Mark Color
+val navigationIcon = Color(0xFFFFFFFF) // Button Icons Color
+val fonts = Color(0xFF630A0A) // Fonts Color
+
+// Dark Mode Farben
+val primaryDarkMode = Color(0xFFC3A4A4) // Primary Color für Dark Mode
+val backgroundColor = Color(0xFF000000) // Hintergrundfarbe in Dark Mode
+val icons = Color(0xFF4F3933) // Icon-Farbe in Dark Mode
+val darkModeFonts = Color(0xFFFFFFFF) // Dark Mode Fonts Color
