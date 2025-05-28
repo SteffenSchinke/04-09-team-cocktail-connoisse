dc4cc4cf5095665d65ef601886e0d908cae969b2
Steffen Schinke <steffen.schinke@gmail.com>
2025-05-28 12:44:30 +0200

    bugfix db onCreate all tables creating

--- app/src/main/java/de/syntax/institut/projectweek/cocktailconnoisse/data/local/CocktailDatabase.kt
+++ app/src/main/java/de/syntax/institut/projectweek/cocktailconnoisse/data/local/CocktailDatabase.kt
@@ -1,16 +1,16 @@
 package de.syntax.institut.projectweek.cocktailconnoisse.data.local
 
 import android.content.Context
+import androidx.core.content.edit
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
 import de.syntax.institut.projectweek.cocktailconnoisse.data.model.Cocktail
 import de.syntax.institut.projectweek.cocktailconnoisse.data.model.Ingredient
+import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import androidx.core.content.edit
-import kotlinx.coroutines.DelicateCoroutinesApi
 
 @Database(entities = [Cocktail::class, Ingredient::class], version = 1, exportSchema = false)
 abstract class CocktailDatabase : RoomDatabase() {
@@ -23,36 +23,31 @@ abstract class CocktailDatabase : RoomDatabase() {
 
         @OptIn(DelicateCoroutinesApi::class)
         fun getDatabase(context: Context): CocktailDatabase {
-            return instance ?:
-                synchronized(this) {
-                    Room.databaseBuilder(
-                        context.applicationContext,
-                        CocktailDatabase::class.java,
-                        "app_cocktails_db"
-                    )
-                        .addCallback(object : RoomDatabase.Callback() {
-                            override fun onCreate(db: SupportSQLiteDatabase) {
-                                super.onCreate(db)
-                                db.execSQL("PRAGMA foreign_keys=ON")
-                            }
-
-                            override fun onOpen(db: SupportSQLiteDatabase) {
-                                super.onOpen(db)
-                                db.execSQL("PRAGMA foreign_keys=ON")
-                            }
-                        })
+            return instance ?: synchronized(this) {
+                Room.databaseBuilder(
+                    context.applicationContext,
+                    CocktailDatabase::class.java,
+                    "app_cocktails_db"
+                )
+                    .addCallback(object : Callback() {
+                        override fun onOpen(db: SupportSQLiteDatabase) {
+                            super.onOpen(db)
+                            db.execSQL("PRAGMA foreign_keys=ON")
+                        }
+                    })
                     .build().also { db ->
-                            instance = db
+                        instance = db
 
-                            GlobalScope.launch {
-                                val prefs = context.getSharedPreferences("init_flags", Context.MODE_PRIVATE)
-                                if (!prefs.getBoolean("db_initialized", false)) {
-                                    db.cocktailDao().clearCachedCocktails()
-                                    db.cocktailDao().clearCachedIngredients()
-                                    prefs.edit { putBoolean("db_initialized", true) }
-                                }
+                        GlobalScope.launch {
+                            val prefs =
+                                context.getSharedPreferences("init_flags", Context.MODE_PRIVATE)
+                            if (!prefs.getBoolean("db_initialized", false)) {
+                                db.cocktailDao().clearCachedCocktails()
+                                db.cocktailDao().clearCachedIngredients()
+                                prefs.edit { putBoolean("db_initialized", true) }
                             }
                         }
+                    }
             }
         }
     }
