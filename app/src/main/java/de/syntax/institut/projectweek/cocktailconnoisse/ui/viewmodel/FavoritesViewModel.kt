3735b38f8a9f01d6b5517dda3a096a851a74552a
Steffen Schinke <steffen.schinke@gmail.com>
2025-05-28 15:58:17 +0200

    bugfix favorites screen

--- app/src/main/java/de/syntax/institut/projectweek/cocktailconnoisse/ui/viewmodel/FavoritesViewModel.kt
+++ app/src/main/java/de/syntax/institut/projectweek/cocktailconnoisse/ui/viewmodel/FavoritesViewModel.kt
@@ -15,39 +15,89 @@ import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class FavoritesViewModel(
 
     application: Application,
-    cocktailRepo: CocktailRepositoryInterface
+    private val cocktailRepo: CocktailRepositoryInterface
 ) : AppBaseViewModelAndroid<ViewModelState>(application, ViewModelState.READY) {
 
     private val _apiError = MutableStateFlow<ApiError?>(null)
     val apiError: StateFlow<ApiError?> = _apiError
 
-    val cocktails = cocktailRepo.getAllFavorites()
-        .onStart { ViewModelState.WORKING }
-        .catch { e ->
-            if( e is ApiError) {
-                _apiError.value = e
-            } else {
-                _apiError.value = ApiError(
-                    type = ApiErrorType.PERSISTENCE_FAILED,
-                    innerMessage = e.localizedMessage ?: "api_error_unknown"
-                )
-            }
-            setState { ViewModelState.ERROR }
-        }
-        .onEach { setState { ViewModelState.READY } }
-        .stateIn(
-            viewModelScope, SharingStarted.WhileSubscribed(5_000L), emptyList()
-        )
+    private val _cocktails = MutableStateFlow<List<Cocktail>>(emptyList())
+    val cocktails: StateFlow<List<Cocktail>> = _cocktails
 
-    fun resetApiError() { _apiError.value = null }
+    init {
+
+        loadFavorites()
+    }
+
+    fun loadFavorites() {
+        viewModelScope.launch {
+            cocktailRepo.getAllFavorites()
+                .onStart {
+                    setState { ViewModelState.WORKING }
+                }
+                .catch { e ->
+                    _apiError.value = if (e is ApiError) {
+                        e
+                    } else {
+                        ApiError(
+                            type = ApiErrorType.PERSISTENCE_FAILED,
+                            innerMessage = e.localizedMessage ?: "api_error_unknown"
+                        )
+                    }
+                    setState { ViewModelState.ERROR }
+                    emit(emptyList()) // Damit collect() nicht abstürzt
+                }
+                .collect { result ->
+                    _cocktails.value = result
+                    setState { ViewModelState.READY }
+                }
+        }
+    }
+
+//    fun loadFavorites(): StateFlow<List<Cocktail>> {
+//
+//        val cocktails = cocktailRepo.getAllFavorites()
+//            .onStart { ViewModelState.WORKING }
+//            .catch { e ->
+//                if (e is ApiError) {
+//                    _apiError.value = e
+//                } else {
+//                    _apiError.value = ApiError(
+//                        type = ApiErrorType.PERSISTENCE_FAILED,
+//                        innerMessage = e.localizedMessage ?: "api_error_unknown"
+//                    )
+//                }
+//                setState { ViewModelState.ERROR }
+//            }
+//            .onEach { setState { ViewModelState.READY } }
+//            .stateIn(
+//                viewModelScope, SharingStarted.WhileSubscribed(5_000L), emptyList()
+//            )
+//
+//        return cocktails
+//    }
+
+    fun resetApiError() {
+        _apiError.value = null
+    }
 
     fun updateIsFavorited(cocktail: Cocktail) {
 
-        //val newCocktail = cocktail.copy(favorited != cocktail.favorited)
-        //cocktailRepo.
+        viewModelScope.launch {
+
+            val newCocktail = cocktail.copy(favorited = !cocktail.favorited)
+            cocktailRepo.updateCachedCocktail(newCocktail)
+
+            cocktailRepo.getAllFavorites()
+                .collect {
+                    _cocktails.value = it
+                    setState { ViewModelState.READY }
+                }
+        }
     }
 }
