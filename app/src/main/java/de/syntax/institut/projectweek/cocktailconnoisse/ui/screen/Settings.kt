17dd4a6a5685d409f673690086dc418c3be2b044
Steffen Schinke <steffen.schinke@gmail.com>
2025-05-31 12:00:59 +0200

    dialog clear cash

--- app/src/main/java/de/syntax/institut/projectweek/cocktailconnoisse/ui/screen/Settings.kt
+++ app/src/main/java/de/syntax/institut/projectweek/cocktailconnoisse/ui/screen/Settings.kt
@@ -22,11 +22,8 @@ import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -34,9 +31,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavHostController
-import de.schinke.steffen.enums.SnackbarDisplayTime
-import de.schinke.steffen.enums.SnackbarMode
-import de.schinke.steffen.extensions.sendMessageOnSnackbar
 import de.schinke.steffen.interfaces.AppRouteContent
 import de.schinke.steffen.interfaces.AppRouteSheet
 import de.schinke.steffen.interfaces.AppRouteTab
