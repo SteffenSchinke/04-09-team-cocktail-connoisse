f818732c70aa6f32c4a7d273878fcfed61b0f957
Steffen Schinke <steffen.schinke@gmail.com>
2025-05-31 20:37:41 +0200

    implement detail screen

--- app/src/main/java/de/syntax/institut/projectweek/cocktailconnoisse/ui/composable/CocktailListByIngredient.kt
+++ app/src/main/java/de/syntax/institut/projectweek/cocktailconnoisse/ui/composable/CocktailListByIngredient.kt
@@ -5,14 +5,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
